a7c18d35bd49497fa6cc57c363d3df4334954af3
Albert Gabitov <agabitov@evrone.com>
2025-10-28 02:14:46 +0300

    Enables image uploads for incidents
    Allows users to upload images when creating or editing incidents.
    
    - Configures `heads_up_web.ex` to serve files from the /uploads directory.
    - Implements image uploading functionality in the incident form, including file selection, drag-and-drop support, progress display, and upload cancellation.
    - Stores uploaded files in the `priv/static/uploads` directory and saves the file path to the incident record.
    - Adds a new `label` component.
    - Adds uploaded directory to .gitignore.

--- lib/heads_up_web/components/core_components.ex
+++ lib/heads_up_web/components/core_components.ex
@@ -419,6 +419,20 @@ defmodule HeadsUpWeb.CoreComponents do
     """
   end
 
+  @doc """
+  Renders a label.
+  """
+  attr :for, :string, default: nil
+  slot :inner_block, required: true
+
+  def label(assigns) do
+    ~H"""
+    <label for={@for} class="block text-sm font-semibold leading-6 text-zinc-800">
+      {render_slot(@inner_block)}
+    </label>
+    """
+  end
+
   ## JS Commands
 
   def show(js \\ %JS{}, selector) do
--- lib/heads_up_web/live/admin/incident_live/form.ex
+++ lib/heads_up_web/live/admin/incident_live/form.ex
@@ -6,16 +6,54 @@ defmodule HeadsUpWeb.Admin.IncidentLive.Form do
   alias HeadsUp.Incidents.Incident
 
   def mount(_params, _session, socket) do
+    socket =
+      socket
+      |> assign(:category_options, Categories.categories_names_and_ids())
+      |> allow_upload(:image,
+        accept: ~w(.png .jpeg .jpg),
+        max_entries: 1,
+        max_file_size: 10_000_000
+      )
+
     {:ok, socket}
   end
 
   def handle_params(params, _uri, socket) do
     socket =
       socket
-      |> assign(:category_options, Categories.categories_names_and_ids())
       |> apply_action(socket.assigns.live_action, params)
 
     {:noreply, socket}
+  end
+
+  def handle_event("validate", %{"incident" => incident_params}, socket) do
+    form = %Incident{} |> Incidents.change_incident(incident_params) |> to_form(action: :validate)
+
+    {:noreply, assign(socket, :form, form)}
+  end
+
+  def handle_event("save", %{"incident" => incident_params}, socket) do
+    uploads_dir = Application.app_dir(:heads_up, "priv/static/uploads")
+    File.mkdir_p!(uploads_dir)
+
+    uploaded_files =
+      consume_uploaded_entries(socket, :image, fn meta, entry ->
+        dest = Path.join(uploads_dir, "#{entry.uuid}-#{entry.client_name}")
+        File.cp!(meta.path, dest)
+        {:ok, ~p"/uploads/#{Path.basename(dest)}"}
+      end)
+
+    incident_params =
+      case uploaded_files do
+        [path] -> Map.put(incident_params, "image_path", path)
+        [] -> incident_params
+      end
+
+    {:noreply, save_incident(socket, socket.assigns.live_action, incident_params)}
+  end
+
+  def handle_event("cancel-upload", %{"ref" => ref}, socket) do
+    {:noreply, cancel_upload(socket, :image, ref)}
   end
 
   defp apply_action(socket, :new, _params) do
@@ -31,16 +69,6 @@ defmodule HeadsUpWeb.Admin.IncidentLive.Form do
     |> assign(:page_title, "Edit Incident")
     |> assign(:incident, incident)
     |> assign(:form, Incidents.change_incident(incident) |> to_form())
-  end
-
-  def handle_event("validate", %{"incident" => incident_params}, socket) do
-    form = %Incident{} |> Incidents.change_incident(incident_params) |> to_form(action: :validate)
-
-    {:noreply, assign(socket, :form, form)}
-  end
-
-  def handle_event("save", %{"incident" => incident_params}, socket) do
-    {:noreply, save_incident(socket, socket.assigns.live_action, incident_params)}
   end
 
   defp save_incident(socket, :new, params) do
@@ -109,7 +137,40 @@ defmodule HeadsUpWeb.Admin.IncidentLive.Form do
           options={@category_options}
         />
 
-        <.input field={@form[:image_path]} label="Image Path" required="true" />
+        <div class="thumbnail">
+          <.input field={@form[:image_path]} label="Image Path" />
+
+          <img src={@incident.image_path} />
+        </div>
+
+        <.label>
+          Add {@uploads.image.max_entries} image
+          (max {trunc(@uploads.image.max_file_size / 1_000_000)} MB)
+        </.label>
+
+        <div class="drop" phx-drop-target={@uploads.image.ref}>
+          <.live_file_input upload={@uploads.image} />
+          <span>or drag and drop here</span>
+        </div>
+        <div :for={entry <- @uploads.image.entries} class="entry">
+          <.live_img_preview entry={entry} />
+
+          <div class="progress">
+            <div class="value">
+              {entry.progress}%
+            </div>
+            <div class="bar">
+              <span style={"width: #{entry.progress}%"}></span>
+            </div>
+            <p :for={err <- upload_errors(@uploads.image, entry)}>
+              {error_to_string(err)}
+            </p>
+          </div>
+
+          <button type="button" phx-click="cancel-upload" phx-value-ref={entry.ref}>
+            &times;
+          </button>
+        </div>
 
         <.button phx-disable-with="Saving...">Save incident</.button>
       </.form>
@@ -118,4 +179,8 @@ defmodule HeadsUpWeb.Admin.IncidentLive.Form do
     </Layouts.app>
     """
   end
+
+  defp error_to_string(:too_large), do: "Too large"
+  defp error_to_string(:too_many_files), do: "You have selected too many files"
+  defp error_to_string(:not_accepted), do: "You have selected an unacceptable file type"
 end
